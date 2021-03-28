970ee6dc45e04c9cb453afd5737345778707187f
Nelson D. Sanchez E <nsanchee@everis.com>
2021-03-28 13:10:54 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PredictionComponent } from './pages/prediction/prediction.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+import { IndicatorComponent } from './pages/indicator/indicator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'prediction',
+    component: PredictionComponent
+  },
+  {
+    path: 'confirmation',
+    component: ConfirmationComponent
+  },
+  {
+    path: 'indicator',
+    component: IndicatorComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+import { PredictionComponent } from './pages/prediction/prediction.component';
+import { IndicatorComponent } from './pages/indicator/indicator.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ConfirmationComponent,
+    PredictionComponent,
+    IndicatorComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
